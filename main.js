26ad5268f166baa3683c8249a09be1fc7842994d
Timothy <tkkibet13@gmail.com>
2022-08-28 22:24:07 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorMessage = document.getElementById('modal').classList.add('hidden')
+const heartClicked = (e) => {
+
+  mimicServerCall()
+    .then(() => {
+      if (e.target.textContent == EMPTY_HEART) {
+        e.target.innerHTML = FULL_HEART
+        e.target.classList.add('activated-heart')
+      } 
+      {
+        e.target.innerHTML = EMPTY_HEART
+        e.target.classList.remove('activated-heart')
+      }
+
+    })
+    .catch(() => {
+      errorMessage.classList.remove('hidden')
+      setTimeout(()=> {
+        errorMessage.classList.add('hidden')
+      }, 3000)
+    })
+
+}
+
+let heartsIcon = document.getElementsByClassName('like-glyph')
+for (let hearts of heartsIcon) {
+  hearts.addEventListener('click', heartClicked)
+}
 
 
 
